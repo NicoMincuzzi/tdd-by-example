e9e5a21880bea37bb4588eb3a6c03a5986bca72c
Nicola Mincuzzi <nicola.mincuzzi@chili.com>
2021-07-14 00:27:15 +0200

    [feat]: Create unit test, which not compile

---
+++ src/test/java/com/nicomincuzzi/tdd/DollarTest.java
@@ -0,0 +1,15 @@
+package com.nicomincuzzi.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DollarTest {
+
+    @Test
+    public void testMultiplication(){
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+}
