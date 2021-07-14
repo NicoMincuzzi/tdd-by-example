73abf572d6fe149edc8d923ed32cf997ee6cae8e
Nicola Mincuzzi <nicola.mincuzzi@chili.com>
2021-07-14 20:49:03 +0200

    [feat]: Return a new object from 'times()'; test fails

--- src/main/java/com/nicomincuzzi/tdd/Dollar.java
+++ src/main/java/com/nicomincuzzi/tdd/Dollar.java
@@ -7,7 +7,8 @@ public class Dollar {
         this.amount = amount;
     }
 
-    public void times(int multiplier) {
+    public Dollar times(int multiplier) {
         amount = amount * multiplier;
+        return null;
     }
 }
