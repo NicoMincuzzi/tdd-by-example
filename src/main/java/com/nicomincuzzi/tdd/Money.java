a303d0ec9b2738fa456579949be824159c2eaa06
Nicola Mincuzzi <nicola.mincuzzi@chili.com>
2021-07-20 23:26:02 +0200

    [feat]: Introduce a Factory method in Money that return Dollar and Franc

--- src/main/java/com/nicomincuzzi/tdd/Money.java
+++ src/main/java/com/nicomincuzzi/tdd/Money.java
@@ -5,6 +5,14 @@ import java.util.Objects;
 public class Money {
     protected int amount;
 
+    public static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    public static Franc franc(int amount) {
+        return new Franc(amount);
+    }
+
     @Override
     public boolean equals(Object obj) {
         Money money = (Money) obj;
--- src/test/java/com/nicomincuzzi/tdd/DollarTest.java
+++ src/test/java/com/nicomincuzzi/tdd/DollarTest.java
@@ -10,14 +10,14 @@ public class DollarTest {
 
     @Test
     public void testMultiplication() {
-        Dollar five = new Dollar(5);
+        Dollar five = Money.dollar(5);
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
     }
 
     @Test
     public void testFrancMultiplication() {
-        Franc five = new Franc(5);
+        Franc five = Money.franc(5);
         assertEquals(new Franc(10), five.times(2));
         assertEquals(new Franc(15), five.times(3));
     }
