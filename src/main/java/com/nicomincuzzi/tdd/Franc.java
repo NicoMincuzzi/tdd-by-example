44ed70d5698fdf16e0ace871f5ce64f7de3b92ec
Nicola Mincuzzi <nicola.mincuzzi@chili.com>
2021-07-20 18:53:07 +0200

    [feat]: Represent Franc, copying Dollar test and Dollar code

---
+++ src/main/java/com/nicomincuzzi/tdd/Franc.java
@@ -0,0 +1,19 @@
+package com.nicomincuzzi.tdd;
+
+public class Franc {
+    private final int amount;
+
+    public Franc(int amount) {
+        this.amount = amount;
+    }
+
+    public Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Franc franc = (Franc) obj;
+        return amount == franc.amount;
+    }
+}
