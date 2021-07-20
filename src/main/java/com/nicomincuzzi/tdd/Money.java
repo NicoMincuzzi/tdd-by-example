f2d56247cdf14ba6b378d472efb83295ec57b6af
Nicola Mincuzzi <nicola.mincuzzi@chili.com>
2021-07-20 23:09:26 +0200

    [feat]: Add check on classes in equals(); bit smelly

--- src/main/java/com/nicomincuzzi/tdd/Money.java
+++ src/main/java/com/nicomincuzzi/tdd/Money.java
@@ -1,11 +1,14 @@
 package com.nicomincuzzi.tdd;
 
+import java.util.Objects;
+
 public class Money {
     protected int amount;
 
     @Override
     public boolean equals(Object obj) {
         Money money = (Money) obj;
-        return amount == money.amount;
+        return amount == money.amount &&
+                Objects.equals(getClass(), money.getClass());
     }
 }
