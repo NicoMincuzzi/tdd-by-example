70b78b6ec6effa463740517e7177abcb637ce2c8
Nicola Mincuzzi <nicolamincuzzi88@gmail.com>
2021-11-23 09:02:29 +0100

    Move exchange rates from Money to Bank

--- src/main/java/com/nicomincuzzi/tdd/Bank.java
+++ src/main/java/com/nicomincuzzi/tdd/Bank.java
@@ -3,6 +3,12 @@ package com.nicomincuzzi.tdd;
 public class Bank {
 
     public Money reduce(Expression source, String to) {
-        return source.reduce(to);
+        return source.reduce(this, to);
+    }
+
+    public int rate(String from, String to) {
+        return (from.equals("CHF") && to.equals("USD"))
+                ? 2
+                : 1;
     }
 }
--- src/main/java/com/nicomincuzzi/tdd/Expression.java
+++ src/main/java/com/nicomincuzzi/tdd/Expression.java
@@ -1,5 +1,5 @@
 package com.nicomincuzzi.tdd;
 
 public interface Expression {
-    Money reduce(String to);
+    Money reduce(Bank bank, String to);
 }
