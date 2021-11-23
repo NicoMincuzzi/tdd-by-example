16a4cdeee1592c6511e2b949e8b61736a8ed05eb
Nicola Mincuzzi <nicolamincuzzi88@gmail.com>
2021-11-23 08:18:34 +0100

    Test Bank.reduce() when the argument is a Money

--- src/test/java/com/nicomincuzzi/tdd/DollarTest.java
+++ src/test/java/com/nicomincuzzi/tdd/DollarTest.java
@@ -60,4 +60,11 @@ public class DollarTest {
         Money result = bank.reduce(sum, "USD");
         assertEquals(Money.dollar(7), result);
     }
+
+    @Test
+    public void testReduceMoney() {
+        Bank bank = new Bank();
+        Money result = bank.reduce(Money.dollar(1), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
 }
