9b0d9e04e1668cf383a49d425b4d4c350d64e5e1
Nicola Mincuzzi <nicolamincuzzi88@gmail.com>
2021-11-30 08:32:56 +0100

    [chore]: Rename file about tests

--- src/test/java/com/nicomincuzzi/tdd/MoneyTest.java
+++ src/test/java/com/nicomincuzzi/tdd/MoneyTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class DollarTest {
+public class MoneyTest {
 
     @Test
     public void testMultiplication() {
